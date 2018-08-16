788d02cf05937fa6a5f77a7cf0115a22310d5063
Sahil Sharma <sahilaug@gmail.com>
2018-08-16 13:54:39 +0530

    adding tests for callbacks and promsises

--- server.test.js
+++ server.test.js
@@ -1,5 +1,19 @@
 import { readFileAsArray, readFileAsArrayUsingPromise } from "./server.js";
+
+test(`read file as array using callback`, async () => {
+  const str = await readFileAsArray("./sample.json", (err, str) => {
+      expect(str.length).not.toBe(0);
+  });
+});
 test(`read file as array using await`, async () => {
   const str = await readFileAsArrayUsingPromise("./sample.json");
   expect(str.length).not.toBe(0);
+});
+
+test(`read file as array using promise`, async () => {
+  readFileAsArrayUsingPromise("./sample.json").then((str) => {
+  expect(str.length).not.toBe(0);
+  }).catch(err => {
+      console.log(err);
+  });
 });
