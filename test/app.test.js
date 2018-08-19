8ed394f012cf939f16ce40fb88c7dc6f77686b71
Sahil Sharma <sahilaug@gmail.com>
2018-08-19 07:37:35 +0530

    reorganizing code

---
+++ test/app.test.js
@@ -0,0 +1,39 @@
+import { readFileAsArray, readFileAsArrayUsingPromise } from "../src/app.js";
+
+// Story1
+test("it returns data as it is ", async () => {
+  const str = await readFileAsArrayUsingPromise("./samples/sample.json");
+  expect(str).toBe(
+    "{name: 'sahil', city: 'bangalore', age: '28', address: { state: 'karnataka'}}"
+  );
+});
+
+test("shouts if when no file is available ", async () => {
+  try {
+    await readFileAsArrayUsingPromise("../samples/sampleFileNotFound.json");
+  } catch (error) {
+    expect(error.length).not.toBe(0);
+  }
+});
+
+//Story2
+test("it should have line number before each line", async () => {
+  const withLineNumbers = true;
+  const str = await readFileAsArrayUsingPromise("./samples/sample.json", {
+    withLineNumbers
+  });
+  expect(str).toBe(
+    "1 {name: 'sahil', city: 'bangalore', age: '28', address: { state: 'karnataka'}}"
+  );
+});
+
+// Story 3
+test("it should prettify the JSON with identations", async () => {
+  const makePretty = true;
+  const str = await readFileAsArrayUsingPromise("./samples/sample1.json", {
+    makePretty
+  });
+  expect(str).toBe(`{
+  "name": "sahil"
+}`);
+});
