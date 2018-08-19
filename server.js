8bb5f230277f393ecf2cd9cd8952a3462407512f
Sahil Sharma <sahilaug@gmail.com>
2018-08-19 07:26:35 +0530

    wire tests and implement 3 user stories

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import beautify from 'json-beautify';
+import jsonFormat from 'json-format';
 
 export const readFileAsArray = (file, cb) => {
   fs.readFile(file, function(err, data) {
@@ -14,20 +16,29 @@ export const readFileAsArray = (file, cb) => {
   });
 };
 
-export const readFileAsArrayUsingPromise = (file, cb = () => {}) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, (err, data) => {
-      if (err) {
-        reject(err);
-        return cb(err);
-      }
+const addLineNumbers = (data ) => data
+               .toString()
+               .trim()
+               .split("\n")
+               .map((line, index) => `${index + 1} ${line}`)
+               .join("\n");
 
-      let lines = data
-        .toString()
-        .trim()
-        .split("\n");
-      resolve(lines);
-      cb(null, lines);
-    });
-  });
-};
+
+const makePretty = data => {
+  return JSON.stringify(JSON.parse(data), null, 2); 
+}
+
+export const readFileAsArrayUsingPromise = (file, config = {withLineNumbers: false, makePretty: false}) => {
+         return new Promise((resolve, reject) => {
+           fs.readFile(file, 'utf8', (err, data) => {
+             if (err) {
+               reject(err);
+               return;
+             }
+             
+            let newLines = config.withLineNumbers ? addLineNumbers(data) : data;
+            newLines = config.makePretty ? makePretty(newLines) : newLines;
+            resolve(newLines.toString());
+           });
+         });
+       };
--- server.test.js
+++ server.test.js
@@ -1,19 +1,32 @@
 import { readFileAsArray, readFileAsArrayUsingPromise } from "./server.js";
 
-test(`read file as array using callback`, async () => {
-  const str = await readFileAsArray("./sample.json", (err, str) => {
-      expect(str.length).not.toBe(0);
-  });
-});
-test(`read file as array using await`, async () => {
-  const str = await readFileAsArrayUsingPromise("./sample.json");
-  expect(str.length).not.toBe(0);
+// // Story1
+test('it returns data as it is ', async () => {
+  const str = await readFileAsArrayUsingPromise('./sample.json');
+  expect(str).toBe("{name: 'sahil', city: 'bangalore', age: '28', address: { state: 'karnataka'}}");
 });
 
-test(`read file as array using promise`, async () => {
-  readFileAsArrayUsingPromise("./sample.json").then((str) => {
-  expect(str.length).not.toBe(0);
-  }).catch(err => {
-      console.log(err);
-  });
+test("shouts if when no file is available ", async () => {
+  try {
+    await readFileAsArrayUsingPromise("./sampleFileNotFound.json");
+  } catch (error) {
+    expect(error.length).not.toBe(0);
+  }
 });
+
+// //Story2
+test('it should have line number before each line', async() => {
+  const withLineNumbers = true;
+  const str = await readFileAsArrayUsingPromise("./sample.json", {withLineNumbers});
+  expect(str).toBe("1 {name: 'sahil', city: 'bangalore', age: '28', address: { state: 'karnataka'}}");
+});
+
+// Story 3
+test('it should prettify the JSON with identations', async () => {
+  const makePretty = true;
+  const str = await readFileAsArrayUsingPromise("./sample1.json", {makePretty});
+  expect(str).toBe(`{
+  "name": "sahil"
+}`);
+} )
+
